d7e41ac92b015c6eac46cdcb10be232ecf396035
Roie A <roee.abram@gmail.com>
2023-08-27 16:03:01 +0300

    UPDATED: OpenAiInquiryController.js
    - Imported StockDao from StockNewsDAO module
    - Removed unnecessary console.log statements
    - Assigned summarized content to req.body.content
    - Updated response to return req.body

--- controllers/OpenAiInquiryController.js
+++ controllers/OpenAiInquiryController.js
@@ -1,11 +1,9 @@
-// Purpose: OpenAiInquiryController handles all requests that are related to OpenAiInquiry.
+StockDao = require("../models/StockNewsDAO");
 
 const { Configuration, OpenAIApi } = require("openai");
 
 class OpenAiInquiryController {
   static async SummarizeOpenAi(req, res) {
-    console.log("Req", req.body.content);
-
     // console.log("Summarizing article content...");
 
     const configuration = new Configuration({
@@ -24,11 +22,10 @@ class OpenAiInquiryController {
           },
         ],
       });
-      console.log(response.data.choices[0].message.content);
+      // console.log(response.data.choices[0].message.content);
+      req.body.content = response.data.choices[0].message.content;
 
-      return res
-        .status(200)
-        .json({ text: response.data.choices[0].message.content });
+      return res.status(200).json(req.body);
     } catch (error) {
       console.error("OpenAI Error:", error.message);
     }
