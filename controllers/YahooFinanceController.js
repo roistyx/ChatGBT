3c83aa1296809333f1b2def7b7a15a32db4bac8e
Roie A <roee.abram@gmail.com>
2023-09-04 10:25:17 +0300

    UPDATE: YahooFinanceController.js
    - Removed async keyword from the HistoricalData method.
    - Added variables for queryOptions and moduleOptions.
    - Added console.log to display "Searching for historical data".
    - Replaced the await keyword with a promise and then() function.
    - Added console.log to display the result.
    - Added catch() function to handle errors.
    - Updated the response status codes in the catch() function.

--- controllers/YahooFinanceController.js
+++ controllers/YahooFinanceController.js
@@ -1,39 +1,31 @@
 const yahooFinance = require("yahoo-finance2").default;
 
 class YahooFinanceController {
-  static async HistoricalData(req, res) {
+  static HistoricalData(req, res) {
     const { symbol, startDate, endDate } = req.formattedObj;
     console.log("req.formattedObj", req.formattedObj);
 
-    try {
-      console.log("Searching for historical data");
+    const queryOptions = {
+      period1: startDate,
+      period2: endDate,
+      interval: "1mo",
+    };
 
-      const queryOptions = {
-        period1: startDate,
-        period2: endDate,
-        interval: "1mo",
-      };
+    const moduleOptions = {
+      modules: ["summaryProfile", "financialData", "calendarEvents"],
+    };
 
-      const moduleOptions = {
-        modules: ["summaryProfile", "financialData", "calendarEvents"],
-      };
+    console.log("Searching for historical data");
 
-      const result = await yahooFinance._chart(
-        symbol,
-        queryOptions,
-        moduleOptions
-      );
-      // const result = await yahooFinance.historical(symbol, queryOptions);
-      // const query = "AAPL";
-      // const queryOptions = { period1: "2023-05-08" /* ... */ };
-      // const result = await yahooFinance._chart(query, queryOptions);
-
-      console.log("result", result);
-
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+    yahooFinance
+      ._chart(symbol, queryOptions, moduleOptions)
+      .then((result) => {
+        console.log("result", result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
   }
 }
 
